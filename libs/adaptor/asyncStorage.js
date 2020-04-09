4b66dcbf619963a385e8950b7b7ceb371cab226e
JosephUz <benkimim11@gmail.com>
2020-04-09 11:25:35 +0300

    - insert işlemine kendi id'sini belirleyebilme eklendi - prototype olarak tanımlanan fonksiyonlar sınıfların içine taşındı

--- libs/adaptor/asyncStorage.js
+++ libs/adaptor/asyncStorage.js
@@ -1,29 +1,29 @@
 function AsyncStorage(RNAsyncStorage) {
     this.RNAsyncStorage = RNAsyncStorage;
-}
 
-AsyncStorage.prototype.setItem = async function (key = "", value = "") {
-    await this.RNAsyncStorage.setItem(key, value);
-}
+    this.setItem = async function (key = "", value = "") {
+        await this.RNAsyncStorage.setItem(key, value);
+    }
 
-AsyncStorage.prototype.getItem = async function (key = "") {
-    return await this.RNAsyncStorage.getItem(key);
-}
+    this.getItem = async function (key = "") {
+        return await this.RNAsyncStorage.getItem(key);
+    }
 
-AsyncStorage.prototype.removeItem = async function (key = "") {
-    await this.RNAsyncStorage.removeItem(key);
-}
+    this.removeItem = async function (key = "") {
+        await this.RNAsyncStorage.removeItem(key);
+    }
 
-AsyncStorage.prototype.getAllKeys = async function () {
-    return await this.RNAsyncStorage.getAllKeys();
-}
+    this.getAllKeys = async function () {
+        return await this.RNAsyncStorage.getAllKeys();
+    }
 
-AsyncStorage.prototype.multiGet = async function (keys = []) {
-    return (await this.RNAsyncStorage.multiGet(keys)).map(x => x[1]);
-}
+    this.multiGet = async function (keys = []) {
+        return (await this.RNAsyncStorage.multiGet(keys)).map(x => x[1]);
+    }
 
-AsyncStorage.prototype.multiRemove = async function (keys = []) {
-    await this.RNAsyncStorage.multiRemove(keys);
+    this.multiRemove = async function (keys = []) {
+        await this.RNAsyncStorage.multiRemove(keys);
+    }
 }
 
 module.exports = AsyncStorage;
--- libs/adaptor/base.js
+++ libs/adaptor/base.js
@@ -1,32 +1,30 @@
 var storage = {};
 
 function Adaptor() {
+    this.setItem = async function (key = "", value = "") {
+        storage[key] = value;
+    }
 
-}
+    this.getItem = async function (key = "") {
+        return storage[key];
+    }
 
-Adaptor.prototype.setItem = async function (key = "", value = "") {
-    storage[key] = value;
-}
+    this.removeItem = async function (key = "") {
+        storage[key] = null;
+        delete storage[key];
+    }
 
-Adaptor.prototype.getItem = async function (key = "") {
-    return storage[key];
-}
+    this.getAllKeys = async function () {
+        return Object.keys(storage);
+    }
 
-Adaptor.prototype.removeItem = async function (key = "") {
-    storage[key] = null;
-    delete storage[key];
-}
+    this.multiGet = async function (keys = []) {
+        return keys.map(key => storage[key]);
+    }
 
-Adaptor.prototype.getAllKeys = async function () {
-    return Object.keys(storage);
-}
-
-Adaptor.prototype.multiGet = async function (keys = []) {
-    return keys.map(key => storage[key]);
-}
-
-Adaptor.prototype.multiRemove = async function (keys = []) {
-    keys.forEach(key => this.removeItem(key));
+    this.multiRemove = async function (keys = []) {
+        keys.forEach(key => this.removeItem(key));
+    }
 }
 
 module.exports = Adaptor;
